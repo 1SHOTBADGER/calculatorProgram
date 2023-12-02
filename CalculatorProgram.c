da034ba014d819442f74934b5329208b946e67f5
Fahad <fahad.alsalihi05@gmail.com>
2023-12-02 13:37:11 -0800

    first commit

---
+++ CalculatorProgram.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+// making a calcultor program started 3 mins ago
+
+int main(){
+
+char operator;
+double num1;
+double num2;
+double result;
+
+printf("\n Choose a oprerator (+ - * /): ");
+scanf("%c", &operator);
+
+printf("Enter number 1 : ");
+scanf("%lf", &num1);l
+
+printf("Enter number 2: ");
+scanf("%lf", &num2);
+
+switch(operator){
+defualt:
+case '+':
+    result = num1
+    printf("%c is not valid", operator);
+
+}
+
+
+
+
+
+
+}
